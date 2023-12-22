d7c8482c533e627aba1aebfe407c9a04f4296202
Phuong LeCong <phuong@lecong.org>
2023-12-21 16:33:28 -0800

    Day 21 Part 2

--- Day21/Day21.cs
+++ Day21/Day21.cs
@@ -2,55 +2,93 @@
 
 using Xunit;
 using AdventOfCode2023.Utils;
+using Xunit.Abstractions;
 
 internal record Tile(char Value, int Row, int Col)
 {
     public bool Valid { get => Value != '#'; }
 }
 
-internal class Solution()
+internal record Coord(int Row, int Col);
+
+internal class Solution(ITestOutputHelper output)
 {
-    public long Part1(IEnumerable<string> input, int steps)
+    public (Tile[][], Coord) Parse(IEnumerable<string> input)
     {
-        Tile[][] grid = input.Select((line, row) => line.Select((value, col) => new Tile(value, row, col)).ToArray()).ToArray();
-        HashSet<Tile> current = grid.SelectMany(row => row.Where(x => x.Value == 'S')).ToHashSet();
+        Tile[][] grid = input
+            .Select((line, row) =>
+                line.Select((value, col) => new Tile(value, row, col)).ToArray())
+            .ToArray();
 
-        for (int i = 0; i < steps; i++)
-        {
-            List<Tile> next = new();
-            foreach (var tile in current)
-            {
-                List<(int Row, int Col)> coords = [
-                    (tile.Row - 1, tile.Col),
-                    (tile.Row + 1, tile.Col),
-                    (tile.Row, tile.Col - 1),
-                    (tile.Row, tile.Col + 1)
-                ];
+        var start = grid
+            .SelectMany(row => row.Where(x => x.Value == 'S').Select(x => new Coord(x.Row, x.Col)))
+            .Single();
 
-                var tileNext = coords
-                    .Where(x => x.Row >= 0 && x.Row < grid.Length)
-                    .Where(x => x.Col >= 0 && x.Col < grid[0].Length)
-                    .Where(x => grid[x.Row][x.Col].Valid)
-                    .Select(x => grid[x.Row][x.Col])
-                    .Distinct();
-
-                next.AddRange(tileNext);
-            }
-            current = next.ToHashSet();
-        }
-
-        return current.Count;
+        return (grid, start);
     }
 
-    public long Part2(IEnumerable<string> input)
+    public Dictionary<Coord, int> BreadthFirstSearch(IEnumerable<string> input)
     {
-        return 0;
+        var (grid, start) = Parse(input);
+
+        Dictionary<Coord, int> distances = new() { { start, 0 } };
+        Queue<(Coord, int)> queue = new();
+        queue.Enqueue((start, 0));
+
+        while (queue.TryDequeue(out var current))
+        {
+            var (coord, distance) = current;
+
+            List<Coord> dirCoords = [
+                new (coord.Row - 1, coord.Col),
+                new (coord.Row + 1, coord.Col),
+                new (coord.Row, coord.Col - 1),
+                new (coord.Row, coord.Col + 1)
+            ];
+
+            var nextCoords = dirCoords
+                .Where(x => x.Row >= 0 && x.Row < grid.Length)
+                .Where(x => x.Col >= 0 && x.Col < grid[0].Length)
+                .Where(x => grid[x.Row][x.Col].Valid)
+                .Distinct();
+
+            foreach (var next in nextCoords)
+            {
+                if (!distances.ContainsKey(next))
+                {
+                    distances.Add(next, distance + 1);
+                    queue.Enqueue((next, distance + 1));
+                }
+            }
+        }
+
+        return distances;
+    }
+
+    public long Part1(IEnumerable<string> input, int steps) =>
+        // figure out all the distances, then count up where coords up to steps and same parity
+        BreadthFirstSearch(input).Values.Count(x => x <= steps && x % 2 == (steps % 2));
+
+    public long Part2(IEnumerable<string> input, long steps)
+    {
+        var (grid, start) = Parse(input);
+
+        // using the logic from https://github.com/villuna/aoc23/wiki/A-Geometric-solution-to-advent-of-code-2023,-day-21
+        var distances = BreadthFirstSearch(input).Values;
+
+        var tiles = (steps - start.Row) / grid.Length;
+        var oddTiles = (long)Math.Pow(tiles + 1, 2) * distances.Count(x => x % 2 == 1);
+        var evenTiles = (long)Math.Pow(tiles, 2) * distances.Count(x => x % 2 == 0);
+        var oddCorners = (tiles + 1) * distances.Count(x => x % 2 == 1 && x > 65);
+        var evenCorners = tiles * distances.Count(x => x % 2 == 0 && x > 65);
+
+        return oddTiles + evenTiles + evenCorners - oddCorners;
     }
 }
 
-public class Test()
+public class Test(ITestOutputHelper output)
 {
-    private readonly Solution solution = new();
+    private readonly Solution solution = new(output);
 
     public IEnumerable<string> Sample
     {
@@ -84,5 +122,5 @@ public class Test()
 
     [Fact]
     public void Part2() =>
-        Assert.Equal(0, solution.Part2(Input));
+        Assert.Equal(601_441_063_166_538, solution.Part2(Input, 26501365));
 }
